791c9ed162c43f747ec257fd303b9235c110b133
Paul C <p.curley@waracle.com>
2015-12-14 20:02:40 +0000

    Add migration script using babel

---
+++ migrate-to-es5.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var babel = require("babel-core");
+var fs = require('fs');
+
+var glob = require("glob")
+var options = {};
+
+glob("{bin,src}/*.js", options, function (er, files) {
+	var i
+	for (i = 0; i < files.length; i++) {
+		babel.transformFile(files[i], function (err, result){
+			if (err) {
+				return console.log(err);
+			}
+			var filename = result.options.filename;
+			fs.writeFile(filename, result.code, function(err) {
+				if(err) {
+					return console.log(err);
+				}
+				console.log(filename + " converted");
+			});
+
+		});
+	}
+})
